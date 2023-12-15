256987b6af49559a9dc913156cd0eba03558f56d
roxoho <as4372056@gmail.com>
2023-12-15 11:48:00 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordion from "./Accordion";
+import { accordionData } from "./utils/content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <div className="accordion">
+      {accordionData.map(({title,content,key}) => {
+        return <Accordion title={title} content={content}
+        key={key} />
+      })}
     </div>
+    </>
   );
 }
 
---
+++ src/utils/content.js
@@ -0,0 +1,52 @@
+export const accordionData = [
+  {
+    key: "1",
+    title: "What Is HTML?",
+    content: `The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.`,
+  },
+  {
+    key: "2",
+    title: "What Is React?",
+    content: `React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies.`,
+  },
+  {
+    key: "3",
+    title: "What Is Node.js?",
+    content: `Node.js is a cross-platform, open-source server environment that can run on Windows, Linux, Unix, macOS, and more. Node.js is a back-end JavaScript runtime environment, runs on the V8 JavaScript Engine, and executes JavaScript code outside a web browser.`,
+  },
+  {
+    key: "4",
+    title: "What Is Golang?",
+    content: `Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. It is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency`,
+  },
+  {
+    key: "5",
+    title: "What Is Python?",
+    content: `Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace.`,
+  },
+  {
+    key: "6",
+    title: "What Is Java?",
+    content: `Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. It is intended to let application developers write once, run anywhere (WORA), meaning that compiled Java code can run on all platforms that support Java without the need for recompilation.`,
+  },
+  {
+    key: "7",
+    title: "What Is TypeScript?",
+    content: `TypeScript is a superset of JavaScript that adds static typing to the language. It was developed by Microsoft and is designed for the development of large applications and can be used to develop JavaScript applications for both client-side and server-side execution.`,
+  },
+  {
+    key: "8",
+    title: "What Is Angular?",
+    content: `Angular is a platform and framework for building client-side applications with HTML, CSS, and JavaScript/TypeScript. It is maintained by Google and is designed to make both the development and testing of such applications easier.`,
+  },
+  {
+    key: "9",
+    title: "What Is Docker?",
+    content: `Docker is a set of platform-as-a-service products that use OS-level virtualization to deliver software in packages called containers. Containers are isolated from one another and bundle their software, libraries, and configuration files; they can communicate with each other through well-defined channels.`,
+  },
+  {
+    key: "10",
+    title: "What Is RESTful API?",
+    content: `RESTful (Representational State Transfer) is an architectural style for designing networked applications. A RESTful API (Application Programming Interface) is an interface that follows the principles of REST. It is a set of rules that developers follow when they create their API.`,
+  },
+];
